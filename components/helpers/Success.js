524cf061451fadcd17b6f0c39bdb25d0940f4373
Vansh Chinda <45889753+vanshchinda@users.noreply.github.com>
2023-04-05 05:57:33 +0530

    lottie not working added gif for now

--- components/helpers/Success.js
+++ components/helpers/Success.js
@@ -1,37 +1,53 @@
-import { Image, View, Text } from 'react-native'
-import React, { useEffect } from 'react'
-import bidSuccess from "../../assets/images/common/bidSuccess.png"
-import { useNavigation } from '@react-navigation/native'
+import React, { useEffect, useRef, useState } from "react";
+import { Image, View, Text, Animated, Easing } from "react-native";
+// import bidSuccess from "../../assets/images/common/bidSuccess.png";
+import { useNavigation } from "@react-navigation/native";
+import successLottie from "../../assets/lottie/successful.gif";
 
 export default function Success() {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
   useEffect(() => {
     const time = setTimeout(() => {
       navigation.goBack();
-    }, 1500)
+    }, 1500);
 
     return () => {
-      clearTimeout(time)
-    }
-  }, [])
+      clearTimeout(time);
+    };
+  }, []);
+
+  const animation = useRef(null);
+  useEffect(() => {
+    animation.current?.play();
+  }, []);
 
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    }}>
-      <Image source={bidSuccess}
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image style={{ width: 300, height: 200 }} source={successLottie} />
+
+      {/* <Image
+        source={bidSuccess}
         style={{
           width: "40%",
           height: "40%",
           aspectRatio: 1.2,
-        }} />
-      <Text style={{
-        fontSize: 20,
-        fontWeight: "bold"
-      }}>Success!</Text>
+        }}
+      /> */}
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "bold",
+        }}
+      >
+        Success!
+      </Text>
     </View>
-  )
+  );
 }
